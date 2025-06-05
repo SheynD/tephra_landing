98198b9304e97a04c562d187cc66c6f0f471131e
Sheyn Denizov <sheynvip16@gmail.com>
2025-06-04 22:59:08 -0400

    Update subscribe.js

--- api/subscribe.js
+++ api/subscribe.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
               channels: {
                 email: "consent"
               },
-              person: {
+              profile: {
                 email,
                 first_name,
                 last_name
